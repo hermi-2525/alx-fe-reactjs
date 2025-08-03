b3d0b4ba3b660aad6609708b1b68d410cf9ea9ee
hermi <hermelaashagre5@gmail.com>
2025-08-03 23:09:19 +0300

    Add recipe-sharing-app with Zustand setup

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <hr />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
